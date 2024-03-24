8775a7300fb94bad9db1053dce9bd7cf78c45abc
Gtrust99 <150854590+Gtrust99@users.noreply.github.com>
2024-03-24 11:26:18 +0100

    Add files via upload

--- Assets/Scripts/WasteController/WasteDataHolder.cs
+++ Assets/Scripts/WasteController/WasteDataHolder.cs
@@ -3,4 +3,14 @@ using UnityEngine;
 public class WasteDataHolder : MonoBehaviour
 {
     public Waste wasteData;
+    private void Update()
+    {
+        if(transform.position.x>=Cameraview.Instance.maxcamera)
+        {
+            Destroy(gameObject);
+            Debug.Log("Distrutto");
+        }
+
+        
+    }
 }
