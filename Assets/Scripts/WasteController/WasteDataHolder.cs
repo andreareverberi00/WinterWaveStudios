bb2ad1c5127852dd44f52420581cb692fffaf3fd
Gtrust99 <150854590+Gtrust99@users.noreply.github.com>
2024-03-26 17:14:43 +0100

    Add files via upload

--- Assets/Scripts/WasteController/WasteDataHolder.cs
+++ Assets/Scripts/WasteController/WasteDataHolder.cs
@@ -17,12 +17,14 @@ public class WasteDataHolder : MonoBehaviour
                 {
                     Debug.Log("Correct sorting!");
                     ScoreController.Instance.AddScore();
+                    //ScoreController.Instance.amount=ScoreController.Instance.amount + 5;
                 }
                 else
                 {
                     Debug.Log("Incorrect sorting.");
                     //ScoreController.Instance.RemoveScore();
                     BatteryController.Instance.ConsumeEnergy();
+                    //ScoreController.Instance.amount=ScoreController.Instance.startedamount;
                 }
             }
         }
