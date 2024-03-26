66012a1ce0936ba6a9da7234cfcf002ead9f7839
diegogurr <diegogurrieri@live.com>
2024-03-26 13:46:35 +0100

    Energy decrease over time

--- Assets/Scripts/Battery/BatteryController.cs
+++ Assets/Scripts/Battery/BatteryController.cs
@@ -6,6 +6,10 @@ public class BatteryController : MonoBehaviour
     public int currentEnergy { get; private set; }
     public int maxEnergy = 100;
     public int consumeEnergyAmount = 20;
+
+    public float energyDrainRate = 1f; // Quanta energia viene consumata ogni secondo.
+    public float energyDrainInterval = 1f; // Intervallo in secondi a cui l'energia si riduce.
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -16,17 +20,21 @@ public class BatteryController : MonoBehaviour
         {
             Instance = this;
         }
-        currentEnergy = maxEnergy; // Energia iniziale al massimo.
-
+        currentEnergy = maxEnergy;
+        InvokeRepeating("DrainEnergy", energyDrainInterval, energyDrainInterval);
+    }
+    private void DrainEnergy()
+    {
+        // Riduce l'energia basata sul tasso di consumo e aggiorna l'UI
+        currentEnergy = Mathf.Max(currentEnergy - Mathf.RoundToInt(energyDrainRate * energyDrainInterval), 0);
+        UIController.Instance.UpdateEnergy(currentEnergy);
+        CheckForGameOver();
     }
 
     public void CollectBattery(int energyAmount)
     {
         currentEnergy = Mathf.Min(currentEnergy + energyAmount, maxEnergy);
         UIController.Instance.UpdateEnergy(currentEnergy);
-
-        // Qui, aggiorni l'UI dell'energia. Per esempio:
-        // UIController.Instance.UpdateEnergy(currentEnergy);
         CheckForGameOver();
     }
 
@@ -39,13 +47,10 @@ public class BatteryController : MonoBehaviour
         }
     }
 
-    // Chiamato quando il giocatore consuma energia.
     public void ConsumeEnergy()
     {
         currentEnergy = Mathf.Max(currentEnergy - consumeEnergyAmount, 0);
         UIController.Instance.UpdateEnergy(currentEnergy);
-        // Aggiorna l'UI dell'energia
-        // UIController.Instance.UpdateEnergy(currentEnergy);
         CheckForGameOver();
     }
 }
