343112cd23bf1fee36181ab8eb64ffb9025b308c
Gtrust99 <reyegiancarlo@gmail.com>
2024-04-09 15:32:02 +0200

    Add a prototype daily mission

---
+++ Assets/Scripts/PrototypeMission/DailyMissionCollection.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Daily Mission Collection", menuName = "Create Daily Mission Collection")]
+public class DailyCollection : ScriptableObject
+{
+    public List<DM> Missions = new List<DM>();
+
+    public DM GetMission()
+    {
+        return Missions[Random.Range(0, Missions.Count)];
+    }
+}
