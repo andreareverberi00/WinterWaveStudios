4c3249e5fe3180ad36fd262bd994973ff635d868
Gtrust99 <reyegiancarlo@gmail.com>
2024-04-09 23:31:01 +0200

    Finallylightsolved

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class LightController : MonoBehaviour
 {
-   
-    
+    public float rotationSpeed = 30f;
+    public Light spotlight;
+    public Light spotlight2;
+    public GameObject Spotlight;
+    void Start()
+    {
+        //spotlight = GetComponent<Light>(); // Ottieni il componente Light
+    }
+
+    void Update()
+    {
+        // Ruota la luce attorno all'asse Y
+        spotlight.transform.Rotate(Vector3.left, rotationSpeed * Time.deltaTime);
+        spotlight2.transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        if(SpeakerController.Instance.Speed==true)
+        {
+            Spotlight.SetActive(true);
+        }
+        else
+        {
+            Spotlight.SetActive(false);
+        }
+    }
+
 }
