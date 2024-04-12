29c90bea68cc3533b9ce1a530664be3ff1fd06fe
diegogurr <diegogurrieri@live.com>
2024-04-12 22:54:45 +0200

    Reimported power up controller scripts

---
+++ Assets/Scripts/PowerUpController/List/PowerMulti.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerMulti : MonoBehaviour
+{
+    public GameObject Portal;
+    private void OnMouseDown()
+    {
+        transform.position = new Vector3(100, 100, 100);
+        StartCoroutine(MultiplicatorGame());
+        PowerPool.Instance.ReturnPower(gameObject);
+
+    }
+
+
+    IEnumerator MultiplicatorGame()
+    {
+        
+        ScoreController.Instance.amount=ScoreController.Instance.amount * 2;// rallenta il gioco al 50%
+        yield return new WaitForSeconds(10f); // attendi per 10 secondi
+        ScoreController.Instance.amount = ScoreController.Instance.amount / 2;
+        PowerPool.Instance.ReturnPower(gameObject);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Floor"))
+            PowerPool.Instance.ReturnPower(gameObject);
+        if (collision.collider.GetType() == typeof(CapsuleCollider))
+        {
+            //Debug.Log("Collisione con un oggetto che ha un capsule collider");
+            transform.position = new Vector3(Portal.transform.position.x + 0.1f, Portal.transform.position.y, Portal.transform.position.z);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PowerUpController/PowePoolController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowePoolController : MonoSingleton<PowePoolController>
+{
+    [SerializeField] private Transform spawnPosition;
+    [SerializeField] private float spawnInterval = 10f;
+    //[SerializeField, Range(0, 100)] private int spawnSLowProbability = 10;
+
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval + Random.Range(-0.25f, 0.25f));
+            int RandomPowerSpawn = Random.Range(0,101);
+            if(RandomPowerSpawn >=0 && RandomPowerSpawn <= 30)
+            {
+                SpawnSlow();
+            }
+            if (RandomPowerSpawn >= 31 && RandomPowerSpawn <= 60)
+            {
+                SpawnMulti();
+            }
+            if (RandomPowerSpawn >= 31 && RandomPowerSpawn <= 60)
+            {
+                SpawnGravity();
+            }
+        }
+    }
+    void SpawnSlow()
+    {
+        GameObject slow = PowerPool.Instance.GetPower();
+        if (slow != null)
+        {
+            slow.transform.position = spawnPosition.position;
+        }
+    }
+    void SpawnMulti()
+    {
+        GameObject multi = PowerPool.Instance.GetPower();
+        if (multi != null)
+        {
+            multi.transform.position = spawnPosition.position;
+        }
+    }
+    void SpawnGravity()
+    {
+        GameObject gravity = PowerPool.Instance.GetPower();
+        if (gravity != null)
+        {
+            gravity.transform.position = spawnPosition.position;
+        }
+    }
+}
