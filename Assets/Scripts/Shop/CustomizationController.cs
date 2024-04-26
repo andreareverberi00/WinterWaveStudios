8635fae4f65804b1001284aeb2344cdb9b0690a3
diegogurr <diegogurrieri@live.com>
2024-04-26 19:06:34 +0200

    Revert "Merge"
    This reverts commit 728dbe4c206433e41139ab33bf218120cfb4d32a, reversing
    changes made to c3f290a995490f2cb1816163aba9f43571285a8c.

--- Assets/Scripts/Shop/CustomizationController.cs
+++ Assets/Scripts/Shop/CustomizationController.cs
@@ -41,18 +41,18 @@ public class CustomizationController : MonoSingleton<CustomizationController>
     }
     public void Pro_Antenna()
     {
-        ScoreController.Instance.scoreAmount = 20;
+        ScoreController.Instance.scoreAmount = 30;
         BatteryController.Instance.consumeEnergyAmount = 20;
     }
     public void Tank_Antenna()
     {
-        ScoreController.Instance.scoreAmount = 5;
-        BatteryController.Instance.consumeEnergyAmount = 5;
+        ScoreController.Instance.scoreAmount = 3;
+        BatteryController.Instance.consumeEnergyAmount = 1;
     }
     public void Base_Antenna()
     {
         ScoreController.Instance.scoreAmount = 10;
-        BatteryController.Instance.consumeEnergyAmount = 10;
+        BatteryController.Instance.consumeEnergyAmount = 5;
     }
     public void Organic_Perk()
     {
