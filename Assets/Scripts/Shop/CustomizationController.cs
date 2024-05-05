65da4ae6300569cf137db56540899dbe8db53a37
diegogurr <diegogurrieri@live.com>
2024-05-05 10:42:15 +0200

    PlayerPrefs plugin + fix

--- Assets/Scripts/Shop/CustomizationController.cs
+++ Assets/Scripts/Shop/CustomizationController.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class CustomizationController : MonoSingleton<CustomizationController>
 {
-    public int coins; // Monete disponibili per l'utente
+    public int coins;
     void Start()
     {
-        //PlayerPrefs.SetInt("Coins", coins); // Imposta le monete iniziali //da rimuovere perchè è solo per test
-        //Scene_Link2.Instance.proantenna = false;
-        //Scene_Link2.Instance.tankantenna = false;
-        //Scene_Link2.Instance.baseantenna = true;
-        //Scene_Link2.Instance.perkorganic = false;
-        //Scene_Link2.Instance.perkmetal = false;
-        //Scene_Link2.Instance.perkpaper = false;
-        //Scene_Link2.Instance.perkplastic = true;
-        //Scene_Link2.Instance.perkglass = false;
         LoadCoins();
-        
     }
 
     [ContextMenu("Clear Player Prefs")]
@@ -46,82 +36,6 @@ public class CustomizationController : MonoSingleton<CustomizationController>
             PlayerPrefs.SetInt("Coins", 9999);
             UICustomizationController.Instance.UpdateUI();
         }
-    }
-    public void Pro_Antenna()
-    {
-        PlayerPrefs.SetInt("proantenna", Scene_Link2.Instance.proantenna ? 1 : 0);
-        Scene_Link2.Instance.proantenna = PlayerPrefs.GetInt("proantenna", 0) == 1;
-        Scene_Link2.Instance.proantenna = true;
-        Scene_Link2.Instance.tankantenna = false;
-        Scene_Link2.Instance.baseantenna = false;
-
-    }
-    public void Tank_Antenna()
-    {
-        PlayerPrefs.SetInt("tankantenna", Scene_Link2.Instance.tankantenna ? 1 : 0);
-        Scene_Link2.Instance.tankantenna = PlayerPrefs.GetInt("tankantenna", 0) == 1;
-        Scene_Link2.Instance.proantenna = false;
-        Scene_Link2.Instance.tankantenna = true;
-        Scene_Link2.Instance.baseantenna = false;
-    }
-    public void Base_Antenna()
-    {
-        PlayerPrefs.SetInt("baseantenna", Scene_Link2.Instance.baseantenna ? 1 : 0);
-        Scene_Link2.Instance.baseantenna = PlayerPrefs.GetInt("baseantenna", 0) == 1;
-        Scene_Link2.Instance.proantenna = false;
-        Scene_Link2.Instance.tankantenna = false;
-        Scene_Link2.Instance.baseantenna = true;
-    }
-    public void Organic_Perk()
-    {
-        PlayerPrefs.SetInt("perkorganic", Scene_Link2.Instance.perkorganic ? 1 : 0);
-        Scene_Link2.Instance.perkorganic = PlayerPrefs.GetInt("perkorganic", 0) == 1;
-        Scene_Link2.Instance.perkorganic = true;
-        Scene_Link2.Instance.perkmetal = false;
-        Scene_Link2.Instance.perkpaper = false;
-        Scene_Link2.Instance.perkplastic = false;
-        Scene_Link2.Instance.perkglass = false;
-
-    }
-    public void Glass_Perk()
-    {
-        PlayerPrefs.SetInt("perkglass", Scene_Link2.Instance.perkglass ? 1 : 0);
-        Scene_Link2.Instance.perkglass = PlayerPrefs.GetInt("perkglass", 0) == 1;
-        Scene_Link2.Instance.perkorganic = false;
-        Scene_Link2.Instance.perkmetal = false;
-        Scene_Link2.Instance.perkpaper = false;
-        Scene_Link2.Instance.perkplastic = false;
-        Scene_Link2.Instance.perkglass = true;
-    }
-    public void Metal_Perk()
-    {
-        PlayerPrefs.SetInt("perkmetal", Scene_Link2.Instance.perkmetal ? 1 : 0);
-        Scene_Link2.Instance.perkmetal = PlayerPrefs.GetInt("perkmetal", 0) == 1;
-        Scene_Link2.Instance.perkorganic = false;
-        Scene_Link2.Instance.perkmetal = true;
-        Scene_Link2.Instance.perkpaper = false;
-        Scene_Link2.Instance.perkplastic = false;
-        Scene_Link2.Instance.perkglass = false;
-    }
-    public void Plastic_Perk()
-    { 
-        PlayerPrefs.SetInt("perkplastic", Scene_Link2.Instance.perkplastic ? 1 : 0);
-        Scene_Link2.Instance.perkplastic = PlayerPrefs.GetInt("perkplastic", 0) == 1;
-        Scene_Link2.Instance.perkorganic = false;
-        Scene_Link2.Instance.perkmetal = false;
-        Scene_Link2.Instance.perkpaper = false;
-        Scene_Link2.Instance.perkplastic = true;
-        Scene_Link2.Instance.perkglass = false;
-    }
-    public void Paper_Perk()
-    {
-        PlayerPrefs.SetInt("perkpaper", Scene_Link2.Instance.perkpaper ? 1 : 0);
-        Scene_Link2.Instance. perkpaper = PlayerPrefs.GetInt("perkpaper", 0) == 1;
-        Scene_Link2.Instance.perkorganic = false;
-        Scene_Link2.Instance.perkmetal = false;
-        Scene_Link2.Instance.perkpaper = true;
-        Scene_Link2.Instance.perkplastic = false;
-        Scene_Link2.Instance.perkglass = false;
     }
 
 }
--- Assets/Scripts/Shop/SceneLink2.cs
+++ Assets/Scripts/Shop/SceneLink2.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Scene_Link2 : MonoSingleton<Scene_Link2>
@@ -13,30 +11,18 @@ public class Scene_Link2 : MonoSingleton<Scene_Link2>
     public bool perkplastic;
     public bool perkorganic;
     public bool perkmetal;
+
     void Start()
     {
+        baseantenna = PlayerPrefs.GetString("SelectedAntenna") == "AntennaBase";
+        tankantenna = PlayerPrefs.GetString("SelectedAntenna") == "AntennaTank";
+        proantenna = PlayerPrefs.GetString("SelectedAntenna") == "AntennaPro";
 
-        PlayerPrefs.SetInt("baseantenna", baseantenna ? 1 : 0);
-        baseantenna = PlayerPrefs.GetInt("baseantenna", 0) == 1;
-        PlayerPrefs.SetInt("tankantenna", tankantenna ? 1 : 0);
-        tankantenna = PlayerPrefs.GetInt("tankantenna", 0) == 1;
-        PlayerPrefs.SetInt("proantenna", proantenna ? 1 : 0);
-        proantenna = PlayerPrefs.GetInt("proantenna", 0) == 1;
-        PlayerPrefs.SetInt("perkpaper", perkpaper ? 1 : 0);
-        perkpaper = PlayerPrefs.GetInt("perkpaper", 0) == 1;
-        PlayerPrefs.SetInt("perkglass", perkglass ? 1 : 0);
-        perkglass = PlayerPrefs.GetInt("perkglass", 0) == 1;
-        PlayerPrefs.SetInt("perkplastic", perkplastic ? 1 : 0);
-        perkplastic = PlayerPrefs.GetInt("perkplastic", 0) == 1;
-        PlayerPrefs.SetInt("perkorganic", perkorganic ? 1 : 0);
-        perkorganic = PlayerPrefs.GetInt("perkorganic", 0) == 1;
-        PlayerPrefs.SetInt("perkmetal", perkmetal ? 1 : 0);
-        perkmetal = PlayerPrefs.GetInt("perkmetal", 0) == 1;
-
-
+        perkpaper = PlayerPrefs.GetString("SelectedPerk") == "PaperPerk";
+        perkglass = PlayerPrefs.GetString("SelectedPerk") == "GlassPerk";
+        perkplastic = PlayerPrefs.GetString("SelectedPerk") == "PlasticPerk";
+        perkorganic = PlayerPrefs.GetString("SelectedPerk") == "OrganicPerk";
+        perkmetal = PlayerPrefs.GetString("SelectedPerk") == "MetalPerk";
     }
-    //private void Update()
-    //{
-    //    Debug.Log(ScoreController.Instance.scoreAmount);
-    //}
+
 }
